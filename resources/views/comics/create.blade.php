cac53fa9b2aed8ae03ecf850c005050e6d71a299
Evanflow89 <ivanbaragone@gmail.com>
2022-07-16 11:49:36 +0200

    wip create form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,4 +6,20 @@
 
 @section('page-content')
   <h1>Insert a new Comic</h1>  
+
+  <form>
+    <div class="mb-3">
+      <label for="title" class="form-label">Title</label>
+      <input type="text" class="form-control" id="title" name="title">
+    </div>
+   
+    <div class="mb-3">
+    <select class="form-select">
+      <option value="comic book">Comic Book</option>
+      <option value="graphic novel">Graphic Novel</option>
+    </select>
+    </div>
+ 
+    <button type="submit" class="btn btn-primary">Insert</button>
+  </form>
 @endsection
