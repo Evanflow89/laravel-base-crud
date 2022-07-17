884419463d84e507add93409ddeb8016a1150ca7
Evanflow89 <ivanbaragone@gmail.com>
2022-07-17 10:53:20 +0200

    Added Route Edit

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -77,6 +77,7 @@ class ComicController extends Controller
     public function edit(Comic $comic)
     {
        return view('comics.edit', compact('comic'));
+       
     }
 
     /**
@@ -86,9 +87,13 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Comic $comic)
     {
-        //
+        $data = $request->all();
+
+        $comic->update($data);
+
+        return redirect()->route('comics.show', $comic->id);
     }
 
     /**
